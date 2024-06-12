494b62a15f5de03871c4b042b189c66949716a2b
Renan Rivadeneira <renan.rivadeneira@experteam.com.ec>
2024-06-12 12:48:25 -0500

    feat: added first day of interfaces configuration

--- src/Commands/GenerateInterfacesCommand.php
+++ src/Commands/GenerateInterfacesCommand.php
@@ -7,21 +7,30 @@ use Illuminate\Support\Carbon;
 
 abstract class GenerateInterfacesCommand extends Command
 {
+    public int $startDayOfMonth = 0;
 
-    public function getInterfaceOptions(?string $interfaceType = null): array
+    public function getInterfaceOptions(?string $interfaceType = null): false|array
     {
         $from = $this->option('from');
         $to = $this->option('to');
         $toSftp = $this->option('toSftp');
 
-        $from = !is_null($from) ? Carbon::create($from) : Carbon::yesterday()->startOfDay();
-        $to = !is_null($to) ? Carbon::create($to) : Carbon::yesterday()->endOfDay();
+        if (is_null($from) && is_null($to) && $this->startDayOfMonth > Carbon::now()->format('d')) {
+            return false;
+        } elseif (is_null($from) && is_null($to) && $this->startDayOfMonth == Carbon::now()->format('d')) {
+            $from = Carbon::now()->startOfMonth()->startOfDay();
+            $to = Carbon::yesterday()->endOfDay();
+        } else {
+            $from = !is_null($from) ? Carbon::create($from) : Carbon::yesterday()->startOfDay();
+            $to = !is_null($to) ? Carbon::create($to) : Carbon::yesterday()->endOfDay();
+        }
+
         $toSftp = is_null($toSftp) || $toSftp == 'true';
 
         return [
             'from' => $from->format('Y-m-d H:m:s'),
             'to' => $to->format('Y-m-d H:m:s'),
-            'transaction_id' => \Str::orderedUuid(),
+            'transaction_id' => \Str::orderedUuid()->toString(),
             'status' => 0,
             'to_sftp' => $toSftp,
             'type' => $interfaceType,
